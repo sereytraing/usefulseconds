2fc3a72e154c0e42b4f10e51aecf26f38b11c50c
TRAING Serey <t.sereypich@gmail.com>
2017-06-10 10:53:33 +0200

    add rewarded video test

--- app/src/main/java/com/example/alkrox/usefulseconds/MainActivity.java
+++ app/src/main/java/com/example/alkrox/usefulseconds/MainActivity.java
@@ -2,12 +2,92 @@ package com.example.alkrox.usefulseconds;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+public class MainActivity extends AppCompatActivity implements  RewardedVideoAdListener{
+
+    public static final String APP_ID = "ca-app-pub-7274580795432430~2031062709";
+    public static final String ANNOUNCE_ID = "ca-app-pub-3940256099942544/5224354917";
+    //ca-app-pub-3940256099942544/5224354917 GOOGLE TEST
+    //ca-app-pub-7274580795432430/1564901102 LE NOTRE
+    private RewardedVideoAd mAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, APP_ID);
+
+        Button button_ad_rewarded_video = (Button) findViewById(R.id.button_ad_reward_video);
+        button_ad_rewarded_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRewardedVideo();
+            }
+        });
+
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+
+        loadRewardedVideoAd();
+
+    }
+
+    private void startRewardedVideo() {
+        if (mAd.isLoaded()) {
+            mAd.show();
+        }
+    }
+
+    private void loadRewardedVideoAd() {
+        mAd.loadAd(ANNOUNCE_ID, new AdRequest.Builder().build() );
+    }
+
+    // Required to reward the user.
+    @Override
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        // Reward the user.
+    }
+
+    // The following listener methods are optional.
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
     }
 }
