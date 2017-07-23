9a2824e2c798f7887db8dfc8d9d07effff1128d7
Vincent OFFROY <offroy.vincent@gmail.com>
2017-07-23 02:40:04 +0200

    WidgetBases

---
+++ app/src/main/java/com/example/alkrox/usefulseconds/WidgetGroupConfigure.java
@@ -0,0 +1,11 @@
+package com.example.alkrox.usefulseconds;
+
+import android.app.Activity;
+
+/**
+ * Created by vincent on 23/07/2017.
+ */
+
+public class WidgetGroupConfigure extends Activity {
+
+}
